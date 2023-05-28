9aaba3c40eeb46ef117a98bed6834ba1b6ce04ba
i-adarsh <i.adarsh.kr@icloud.com>
2023-05-28 09:30:24 +0530

    Using State

--- tbu-react/src/App.js
+++ tbu-react/src/App.js
@@ -1,21 +1,35 @@
+import { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
 function App() {
-  const state = {
-    persons: [
-      { name: "Adarsh", age: 23},
-      { name: "Sargun", age: 22},
-      { name: "Alex", age: 25},
-      { name: "Sophie", age:24}
 
+  const [state, setState] = useState({
+      persons: [
+      { name: "Adarsh", age: 23 },
+      { name: "Sargun", age: 22 },
+      { name: "Alex", age: 25 },
+      { name: "Sophie", age:24 }
     ]
+    }
+  )
+
+  const switchNameHandler = () => {
+    setState({
+      persons: [
+      { name: "Adarsh", age: 23 },
+      { name: "Sargun", age: 22 },
+      { name: "Sophie", age:21 },
+      { name: "Alex", age: 25 }
+      ]
+    })
   }
+
   return (
     <div className="App">
       <h1>Hello from React App</h1>
       <p>This is really working</p>
-      <button>Switch Name</button>
+      <button onClick={switchNameHandler}>Switch Name</button>
       <Person name={state.persons[0].name} age={state.persons[0].age}/>
       <Person name={state.persons[1].name} age={state.persons[1].age}/>
       <Person name={state.persons[2].name} age={state.persons[2].age}>My hobbies are coding, swimming, martial arts</Person>
